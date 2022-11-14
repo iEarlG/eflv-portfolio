7afc1510a198bd39f5ffee6fe57d93d36a8a8c9b
iEarlG <91045673+iEarlG@users.noreply.github.com>
2022-11-14 14:19:09 +0800

    with icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
